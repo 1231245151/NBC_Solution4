fe631ececd372185938487209b8efc17b4bffa91
1231245151 <homingi@naver.com>
2026-03-11 16:35:32 +0900

    whoat
    ???

--- AlchemyWorkshop.h
+++ AlchemyWorkshop.h
@@ -7,24 +7,37 @@
 using namespace std;
 
 #include "PotionRecipe.h"
+#include "RecipeManager.h"
+#include "StockManager.h"
 
 // AlchemyWorkshop 클래스: 레시피 목록을 관리
 class AlchemyWorkshop
 {
 private:
-    vector<PotionRecipe> recipes;
+    RecipeManager recipeManager_;
+    StockManager stockManager_;
 
 public:
     // addRecipe 메서드: 재료 목록(vector)을 매개변수로 받도록 수정
     void addRecipe(const string& name, const vector<string>& ingredients)
     {
-        recipes.push_back(PotionRecipe(name, ingredients));
-        cout << ">> 새로운 레시피 '" << name << "'이(가) 추가되었습니다." << endl;
+        bool overlap = recipeManager_.AddRecipe(name, ingredients);
+        // 중복된 레시피등록 시 판정
+        if (overlap)
+        {
+            cout << ">> 중복된 레시피입니다. '" << name << "'이(가) 기존 레시피에서 덮어씁니다." << endl;
+        }
+        else
+        {
+            cout << ">> 새로운 레시피 '" << name << "'이(가) 추가되었습니다." << endl;
+        }
     }
 
     // 모든 레시피 출력 메서드
     void displayAllRecipes() const
     {
+        const vector<PotionRecipe>& recipes = recipeManager_.GetAllRecipe();
+
         if (recipes.empty())
         {
             cout << "아직 등록된 레시피가 없습니다." << endl;
@@ -42,76 +55,40 @@ public:
     // 포션 이름 검색
     PotionRecipe SearchCopyRecipeByName(string _name)
     {
-        PotionRecipe SearchRecipe;
+        PotionRecipe* SearchRecipe = recipeManager_.FindRecipeByName(_name);
 
-        // 애초에 레시피잇어야 작동
-        if (recipes.empty())
+        if (SearchRecipe == nullptr)
         {
-            cout << "아직 등록된 레시피가 없습니다." << endl;
-            return SearchRecipe;
+            cout << "해당 레시피는 존재하지 않습니다." << endl;
+
+            // 더미 데이터 반환
+            return PotionRecipe();
         }
 
-        bool search = false;
+         SearchRecipe->displayRecipe();
 
-        // 배열을 앞에서부터 찾아간다(중복은 없다는 가정)
-        for (vector<PotionRecipe>::iterator it = recipes.begin(); it != recipes.end(); ++it)
-        {
-            if (it->GetName() == _name)
-            {
-                SearchRecipe = (*it);
-                cout << "해당 포션 레시피를 발견하였습니다. " << endl;
-                SearchRecipe.displayRecipe();
-
-                search = true;
-                break;
-            }
-        }
-
-        // 레시피가 없다면
-        if(!search)
-        {
-            cout << "해당 포션 레시피는 존재하지 않습니다" << endl;
-        }
-
-        //레시피가 발견되었다면 해당 레시피 반환
-        return SearchRecipe;
+         //레시피가 발견되었다면 해당 레시피 반환
+         return *SearchRecipe;
     }
 
 
     // 포션 재료 검색
     vector<PotionRecipe> SearchCopyRecipeByingred(string _name)
     {
-        // 발견된 레시피 전부 보낼것이기에 배열로 설정
-        vector<PotionRecipe> SearchRecipes;
+        // 현재 제작중
+        vector<PotionRecipe> SearchRecipes = recipeManager_.FindRecipeByingredients(_name);
 
-        // 애초에 레시피잇어야 작동
-        if (recipes.empty())
+        if (SearchRecipes.empty())
         {
-            cout << "아직 등록된 레시피가 없습니다." << endl;
+            cout << "해당 레시피는 존재하지 않습니다." << endl;
+
             return SearchRecipes;
         }
 
-        // 배열을 앞에서부터 찾아간다
-        for (const PotionRecipe& it : recipes)
+        for (PotionRecipe pr : SearchRecipes)
         {
-            const vector<string>& ingredients = it.Getingredients();
-
-            //재료검색
-            for (const string& stringit : ingredients)
-            {
-                // 해당재료가진 레시피 발견햇으면 해당레시피로 저장후 나오기
-                if (stringit == _name)
-                {
-                    SearchRecipes.push_back(it);
-                    it.displayRecipe();
-                    break;
-                }
-            }
-
+            pr.displayRecipe();
         }
-
-        if(SearchRecipes.empty())
-            cout << "해당 재료를 가진 레시피는 존재하지않습니다." << endl;
 
         return SearchRecipes;
     }
